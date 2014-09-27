dbbf21c6d134d945b83ac0ee38f3ea3d69c1efa0
byverdu <byverdu@gmail.com>
2014-09-27 18:48:12 +0100

    CashRegister gets total price

--- test/CashRegisterSpec.js
+++ test/CashRegisterSpec.js
@@ -37,19 +37,16 @@ describe('CashRegister', function() {
 
 	describe('Manipulating the basket', function() {
 
-		describe('For the total amount for', function() {
+		describe('For the total amount of', function() {
 			
 			it('one product', function() {
-				
 				cashRegister.acceptProd(rice);
 
 				expect(cashRegister.basket).to.have.length(1);
 				expect(cashRegister.total_products).to.equal(1)
-
 			});
 
 			it('two products', function() {
-				
 				cashRegister.acceptProd(rice);
 				cashRegister.acceptProd(beans);
 
@@ -60,14 +57,12 @@ describe('CashRegister', function() {
 		describe('For the total quantity if there is', function() {
 			
 			it('only one product', function() {
-				
 				cashRegister.acceptProd(rice);
 
 				expect(cashRegister.total_quantity).to.equal(1)
 			});
 
 			it('more than one product', function() {
-
 				cashRegister.total_quantity = 0;
 				
 				cashRegister.acceptProd(rice);
@@ -80,7 +75,6 @@ describe('CashRegister', function() {
 		describe('Deleting items', function() {
 			
 			it('the last one added', function() {
-				
 				cashRegister.acceptProd(rice);
 				cashRegister.delete_last();
 
@@ -97,5 +91,45 @@ describe('CashRegister', function() {
 				expect(cashRegister.basket).to.contain({name:'beans',price:1.25,quantity:2});
 			});
 		});
+
+	describe('Getting the price', function() {
+		
+		it('for only one product', function() {
+			cashRegister.acceptProd(rice);
+
+      cashRegister.processOrder();
+
+      expect(cashRegister.total_price).to.equal(3.5)
+		});
+
+		it('for one product with a quantity of 2', function() {
+			cashRegister.acceptProd(beans);
+
+			cashRegister.processOrder();
+
+			expect(cashRegister.total_price).to.equal(2.5)
+		});
+
+		it('for two product', function() {
+			var bread = new Product('bread',0.5,1);
+
+			cashRegister.acceptProd(rice);
+			cashRegister.acceptProd(bread);
+
+			cashRegister.processOrder();
+
+			expect(cashRegister.total_price).to.equal(4)
+		});
+
+		it('for two product with different quantities', function() {
+
+			cashRegister.acceptProd(rice);
+			cashRegister.acceptProd(beans);
+
+			cashRegister.processOrder();
+
+			expect(cashRegister.total_price).to.equal(6)
+		});
+	});
 	});
 });
